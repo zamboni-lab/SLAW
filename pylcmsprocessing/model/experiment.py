7feca0ec86155a1753cc7a206e69b700d02cd5e0
Nicola Zamboni <34441841+htmonkey@users.noreply.github.com>
2022-11-01 02:25:45 +0100

    Update experiment.py

--- pylcmsprocessing/model/experiment.py
+++ pylcmsprocessing/model/experiment.py
@@ -1006,12 +1006,9 @@ class Experiment:
                 elif os.path.exists(pwaste):
                     os.remove(pwaste)
 
-    def export_mztab(self, path_db, mztab_format):
+    def export_mztab(self, mztab_format):
         ###Generates a mzTab
-        self.close_db()
-        self.conn = sqlite3.connect(self.path_save_db)
-        self.conn.execute("PRAGMA foreign_keys = 1")
-        c = self.conn.cursor()
+        self.open_db()
         c.execute("SELECT * FROM peakpicking")
         all_peakpicking = c.fetchall()
         self.close_db()
