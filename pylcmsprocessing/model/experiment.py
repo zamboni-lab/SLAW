df024d3d27845e6a5c77f5cb1032201c104a6fd8
Nicola Zamboni <34441841+htmonkey@users.noreply.github.com>
2022-11-02 13:56:17 +0100

    Update experiment.py

--- pylcmsprocessing/model/experiment.py
+++ pylcmsprocessing/model/experiment.py
@@ -1041,7 +1041,7 @@ class Experiment:
             counter += 1
         if counter != 0:
             exporters = exporters[0:counter]
-            clis = [eee.command() for eee in exporters]
+            clis = [eee.command_line() for eee in exporters]
             runner = pr.ParallelRunner(1)
             if len(clis) > 0:
                 runner.run(clis, silent=True)
